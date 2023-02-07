93983cb1fc165d4857f7281d83df7b0eaed2b936
vicmon810 <384572038@qq.com>
2023-02-07 23:15:11 +1300

    inital commit

---
+++ my_SQL.c
@@ -0,0 +1,36 @@
+#include <stab.h>
+
+int main(int argc, char* argv[]){
+    InputBuffer* input_buffer = new_input_buffer();
+    while (true)
+    {
+        print_prompt();
+        read_input(input_buffer);
+
+        if(strcmp(intput_buffer->buffer, ".exit") == 0){
+            cloes_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        }else{
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
+    }
+
+
+}
+
+typedef struct
+{
+    char *buffer;
+    size_t buffer_length;
+    ssize_t input_length;
+} InputBuffer;
+
+InputBuffer *new_input_buffer()
+{
+    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+    input_buffer -> buffer = NULL;
+    input_buffer -> buffer_length = 0;
+    input_buffer -> input_length = 0;
+
+    return input_buffer;
+}
