22edaa818dd7153055fb70ad9bc8fd2e7764b717
vicmon810 <384572038@qq.com>
2023-02-08 22:00:53 +1300

    result doesn't report to the trimnal need double check for it

--- my_SQL.c
+++ my_SQL.c
@@ -10,6 +10,29 @@ typedef struct
     ssize_t input_length;
 } InputBuffer;
 
+typedef enum
+{
+    STATEMENT_INSERT,
+    STATEMENT_SELECT
+} StatementType;
+
+typedef struct
+{
+    StatementType type;
+} Statement;
+
+typedef enum
+{
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+/* new keyword*/
+typedef enum
+{
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
 InputBuffer *new_input_buffer()
 {
     InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
@@ -18,6 +41,46 @@ InputBuffer *new_input_buffer()
     input_buffer->input_length = 0;
 
     return input_buffer;
+}
+/*do_meta_command is just a wrapper for existing functionality that leaves room for more commands*/
+MetaCommandResult do_meta_command(InputBuffer *input_buffer)
+{
+    if (strcmp(input_buffer->buffer, ".exit") == 0)
+    {
+        exit(EXIT_SUCCESS);
+    }
+    else
+    {
+        return META_COMMAND_UNRECOGNIZED_COMMAND;
+    }
+}
+/*prepare_statement AKA SQL Compiler*/
+PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
+{
+    if (strncmp(input_buffer->buffer, "insert", 6) == 0)
+    {
+        statement->type = STATEMENT_INSERT;
+        return PREPARE_SUCCESS;
+    }
+
+    if (strcmp(input_buffer->buffer, "select") == 0)
+    {
+        statement->type = STATEMENT_SELECT;
+        return PREPARE_SUCCESS;
+    }
+}
+
+void execute_statement(Statement *statement)
+{
+    switch (statement->type)
+    {
+    case (STATEMENT_INSERT):
+        printf("This is where we would do an insert.\n");
+        break;
+    case (STATEMENT_SELECT):
+        printf("This is where we would do a select.\n");
+        break;
+    }
 }
 
 void print_prompt()
@@ -62,12 +125,31 @@ int main(int argc, char *argv[])
 
         if (strcmp(input_buffer->buffer, ".exit") == 0)
         {
-            close_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        }
-        else
-        {
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-        }
+            /*refactor main function and suppor two new keywords in the process*/
+            if (input_buffer->buffer[0] == '.')
+            {
+                switch (do_meta_command(input_buffer))
+                {
+                case (META_COMMAND_SUCCESS): /*non-SQL statement like .exit called 'META_COMMAND' */
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
+                    continue;
+                }
+            }
+
+            Statement statement;
+            switch (prepare_statement(input_buffer, &statement))
+            {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                printf("Unrecognized keyword at start of '%s'. \n", input_buffer->buffer);
+                continue;
+            }
+
+            execute_statement(&statement);
+            printf("Executed.\n");
+        } /*-*/
     }
 }
