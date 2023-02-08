75597b761e2b1347158d7e587df2854fd4a68aca
vicmon810 <384572038@qq.com>
2023-02-08 21:20:06 +1300

    YEAH, just made the smallest db in the wrold

--- my_SQL.c
+++ my_SQL.c
@@ -1,22 +1,7 @@
-#include <stab.h>
-
-int main(int argc, char* argv[]){
-    InputBuffer* input_buffer = new_input_buffer();
-    while (true)
-    {
-        print_prompt();
-        read_input(input_buffer);
-
-        if(strcmp(intput_buffer->buffer, ".exit") == 0){
-            cloes_input_buffer(input_buffer);
-            exit(EXIT_SUCCESS);
-        }else{
-            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
-        }
-    }
-
-
-}
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 typedef struct
 {
@@ -27,10 +12,62 @@ typedef struct
 
 InputBuffer *new_input_buffer()
 {
-    InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
-    input_buffer -> buffer = NULL;
-    input_buffer -> buffer_length = 0;
-    input_buffer -> input_length = 0;
+    InputBuffer *input_buffer = (InputBuffer *)malloc(sizeof(InputBuffer));
+    input_buffer->buffer = NULL;
+    input_buffer->buffer_length = 0;
+    input_buffer->input_length = 0;
 
     return input_buffer;
+}
+
+void print_prompt()
+{
+    printf("db > ");
+}
+
+void read_input(InputBuffer *input_buffer)
+{
+    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+    if (bytes_read <= 0)
+    {
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
+    }
+
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+/*
+Proper define function that frees the memory allocated for an instance of inputBuffer*
+*/
+void close_input_buffer(InputBuffer *input_buffer)
+{
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
+/*
+parse  and excute the command
+*/
+
+int main(int argc, char *argv[])
+{
+    InputBuffer *input_buffer = new_input_buffer();
+    while (true)
+    {
+        print_prompt();
+        read_input(input_buffer);
+
+        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        {
+            close_input_buffer(input_buffer);
+            exit(EXIT_SUCCESS);
+        }
+        else
+        {
+            printf("Unrecognized command '%s'.\n", input_buffer->buffer);
+        }
+    }
 }
