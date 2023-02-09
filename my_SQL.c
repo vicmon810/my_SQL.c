cbe46ceb099830a628f7fe73bafc732305546bfc
vicmon810 <384572038@qq.com>
2023-02-09 22:03:01 +1300

    to tired today. bug not yet fixed

--- my_SQL.c
+++ my_SQL.c
@@ -12,6 +12,18 @@ typedef struct
 
 typedef enum
 {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED_COMMAND
+} MetaCommandResult;
+
+typedef enum
+{
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED_STATEMENT
+} PrepareResult;
+
+typedef enum
+{
     STATEMENT_INSERT,
     STATEMENT_SELECT
 } StatementType;
@@ -20,18 +32,7 @@ typedef struct
 {
     StatementType type;
 } Statement;
-
-typedef enum
-{
-    META_COMMAND_SUCCESS,
-    META_COMMAND_UNRECOGNIZED_COMMAND
-} MetaCommandResult;
 /* new keyword*/
-typedef enum
-{
-    PREPARE_SUCCESS,
-    PREPARE_UNRECOGNIZED_STATEMENT
-} PrepareResult;
 
 InputBuffer *new_input_buffer()
 {
@@ -42,11 +43,41 @@ InputBuffer *new_input_buffer()
 
     return input_buffer;
 }
+
+void print_prompt()
+{
+    printf("db > ");
+}
+
+void execute_statement(Statement *statement)
+{
+    switch (statement->type)
+    {
+    case (STATEMENT_INSERT):
+        printf("This is where we would do an insert.\n");
+        break;
+    case (STATEMENT_SELECT):
+        printf("This is where we would do a select.\n");
+        break;
+    }
+}
+
+/*
+Proper define function that frees the memory allocated for an instance of inputBuffer*
+*/
+void close_input_buffer(InputBuffer *input_buffer)
+{
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
+
 /*do_meta_command is just a wrapper for existing functionality that leaves room for more commands*/
+
 MetaCommandResult do_meta_command(InputBuffer *input_buffer)
 {
     if (strcmp(input_buffer->buffer, ".exit") == 0)
     {
+        close_input_buffer(input_buffer);
         exit(EXIT_SUCCESS);
     }
     else
@@ -68,47 +99,7 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
         statement->type = STATEMENT_SELECT;
         return PREPARE_SUCCESS;
     }
-}
-
-void execute_statement(Statement *statement)
-{
-    switch (statement->type)
-    {
-    case (STATEMENT_INSERT):
-        printf("This is where we would do an insert.\n");
-        break;
-    case (STATEMENT_SELECT):
-        printf("This is where we would do a select.\n");
-        break;
-    }
-}
-
-void print_prompt()
-{
-    printf("db > ");
-}
-
-void read_input(InputBuffer *input_buffer)
-{
-    ssize_t bytes_read = getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
-
-    if (bytes_read <= 0)
-    {
-        printf("Error reading input\n");
-        exit(EXIT_FAILURE);
-    }
-
-    input_buffer->input_length = bytes_read - 1;
-    input_buffer->buffer[bytes_read - 1] = 0;
-}
-
-/*
-Proper define function that frees the memory allocated for an instance of inputBuffer*
-*/
-void close_input_buffer(InputBuffer *input_buffer)
-{
-    free(input_buffer->buffer);
-    free(input_buffer);
+    return PREPARE_UNRECOGNIZED_STATEMENT;
 }
 
 /*
@@ -123,33 +114,29 @@ int main(int argc, char *argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if (strcmp(input_buffer->buffer, ".exit") == 0)
+        if (input_buffer->buffer[0] == ".")
         {
-            /*refactor main function and suppor two new keywords in the process*/
-            if (input_buffer->buffer[0] == '.')
+            switch (do_meta_command(input_buffer))
             {
-                switch (do_meta_command(input_buffer))
-                {
-                case (META_COMMAND_SUCCESS): /*non-SQL statement like .exit called 'META_COMMAND' */
-                    continue;
-                case (META_COMMAND_UNRECOGNIZED_COMMAND):
-                    printf("Unrecognized command '%s'\n", input_buffer->buffer);
-                    continue;
-                }
-            }
-
-            Statement statement;
-            switch (prepare_statement(input_buffer, &statement))
-            {
-            case (PREPARE_SUCCESS):
-                break;
-            case (PREPARE_UNRECOGNIZED_STATEMENT):
-                printf("Unrecognized keyword at start of '%s'. \n", input_buffer->buffer);
+            case (META_COMMAND_SUCCESS):
+                continue;
+            case (META_COMMAND_UNRECOGNIZED_COMMAND):
+                printf("Unrecognized command '%s'\n", input_buffer->buffer);
                 continue;
             }
+        }
 
-            execute_statement(&statement);
-            printf("Executed.\n");
-        } /*-*/
-    }
+        Statement statement;
+        switch (prepare_statement(input_buffer, &statement))
+        {
+        case /* constant-expression */ (PREPARE_SUCCESS):
+            /* code */
+            break;
+
+        case (PREPARE_UNRECOGNIZED_STATEMENT):
+            printf("Unrecognized keyword at start of '%s' \n", input_buffer->buffer);
+            break;
+        }
+
+    } /*-*/
 }
