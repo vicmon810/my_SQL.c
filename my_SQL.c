6e8cc63b3c08ed36ef612a92cc8d1a39a6126fcc
vicmon810 <384572038@qq.com>
2023-02-10 22:05:45 +1300

    gain more understand of basic command operation. also SQL storage.

--- my_SQL.c
+++ my_SQL.c
@@ -2,6 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define COLUMN_USERNAME_SIZE 32
+#define COLUMN_EMAIL_SIZE 255
+
+typedef struct
+{
+    uint32_t id;
+    char username[COLUMN_USERNAME_SIZE];
+    char email[COLUMN_EMAIL_SIZE];
+} Row;
+
+/*
+Store rows in blocks of memory called pages
+Each page stores as many rows as it can fit
+Rows are serialized into a compact representation with each page
+Pages are only allocated as needed
+Keep a fixed-size array of pointers to pages
+*/
+#define size_of_attribute(Struct, Attribute) sizeof(((Struct *)0)->Attribute)
+const uint32_t ID_SIZE = size_of_attribute(Row, id);
+const uint32_t USERNAME_SIZE = size_of_attribute(Row, username);
+const uint32_t EMAIL_SIZE = size_of_attribute(Row, email);
+const uint32_t ID_OFFSET = 0;
+const uint32_t USERNAME_OFFSET = ID_OFFSET + ID_SIZE;
+const uint32_t EMAIL_OFFSET = USERNAME_OFFSET + USERNAME_SIZE;
+const uint32_t ROW_SIZE = ID_SIZE + USERNAME_SIZE + EMAIL_SIZE;
 
 typedef struct
 {
@@ -31,6 +56,7 @@ typedef enum
 typedef struct
 {
     StatementType type;
+    Row row_to_insert;
 } Statement;
 /* new keyword*/
 
@@ -49,19 +75,18 @@ void print_prompt()
     printf("db > ");
 }
 
-void execute_statement(Statement *statement)
+void read_input(InputBuffer *input_buffer)
 {
-    switch (statement->type)
+    ssize_t bytes_read = getline((&input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+    if (bytes_read <= 0)
     {
-    case (STATEMENT_INSERT):
-        printf("This is where we would do an insert.\n");
-        break;
-    case (STATEMENT_SELECT):
-        printf("This is where we would do a select.\n");
-        break;
+        printf("Error reading input\n");
+        exit(EXIT_FAILURE);
     }
-}
 
+    input_buffer->input_length = bytes_read - 1;
+    input_buffer->buffer[bytes_read - 1] = 0;
+}
 /*
 Proper define function that frees the memory allocated for an instance of inputBuffer*
 */
@@ -91,6 +116,12 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
     if (strncmp(input_buffer->buffer, "insert", 6) == 0)
     {
         statement->type = STATEMENT_INSERT;
+        int args_assigned = sscanf(input_buffer->buffer, "insert %d %s %s", &(statement->row_to_insert.id),
+                                   statement->row_to_insert.username, statement->row_to_insert.email);
+        if (args_assigned < 3)
+        {
+            return PREPAER_SYNTAX_ERROR;
+        }
         return PREPARE_SUCCESS;
     }
 
@@ -100,6 +131,21 @@ PrepareResult prepare_statement(InputBuffer *input_buffer, Statement *statement)
         return PREPARE_SUCCESS;
     }
     return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement)
+{
+
+    switch (statement->type)
+    {
+    case (STATEMENT_INSERT):
+
+        printf("This is where we would do an insert.\n");
+        break;
+    case (STATEMENT_SELECT):
+        printf("This is where we would do a select.\n");
+        break;
+    }
 }
 
 /*
@@ -114,7 +160,7 @@ int main(int argc, char *argv[])
         print_prompt();
         read_input(input_buffer);
 
-        if (input_buffer->buffer[0] == ".")
+        if (input_buffer->buffer[0] == '.')
         {
             switch (do_meta_command(input_buffer))
             {
@@ -125,18 +171,18 @@ int main(int argc, char *argv[])
                 continue;
             }
         }
-
         Statement statement;
         switch (prepare_statement(input_buffer, &statement))
         {
-        case /* constant-expression */ (PREPARE_SUCCESS):
-            /* code */
+        case (PREPARE_SUCCESS):
             break;
-
         case (PREPARE_UNRECOGNIZED_STATEMENT):
-            printf("Unrecognized keyword at start of '%s' \n", input_buffer->buffer);
-            break;
+            printf("Unrecognized keyword at start of '%s'.\n",
+                   input_buffer->buffer);
+            continue;
         }
+        execute_statement(&statement);
+        printf("Executed.\n");
 
     } /*-*/
 }
